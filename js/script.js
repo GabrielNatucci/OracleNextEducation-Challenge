8913503b54ce0c0f7790801b627e3896645f83d7
JoenneTV <gabriel.natuccirusso@hotmail.com>
2022-01-25 15:29:30 -0300

    correção de bug onde as páginas do footer não abriam

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
-var msg = document.getElementById("msg"); // Seleciona o a caixa de texto onde mostra a mensagem.
 
-document.addEventListener("click", function(event){ // Adiciona um evento à página html.
+var cripto = document.getElementById("btncripto");
+var descripto = document.getElementById("btndescritpo");
+var copiar = document.getElementById("copiar");
+
+cripto.addEventListener("click", function(event){ // Adiciona um evento à página html.
     input = document.getElementById("inputvalor").value;
     event.preventDefault(); // Evita que a página recarregue.
-    let botao = event.target.value; // Lê o valor do botão que foi apertado.
-    if (botao == "Criptografar"){
-        criptografar(input);
-    } else if (botao === "Descriptografar"){
-        descriptografar(input);
-    } else if (botao == "Clique para copiar"){
-        navigator.clipboard.writeText(msg.value)
-    }
-
+    criptografar(input); 
 });
+
+descripto.addEventListener("click", function(event){ // Adiciona um evento à página html.
+    event.preventDefault(); // Evita que a página recarregue.
+    event.preventDefault(); // Evita que a página recarregue.
+    descriptografar(input); 
+});
+
+copiar.addEventListener("click",function(event){
+    var msg = document.getElementById("msg").value; // Seleciona o a caixa de texto onde mostra a mensagem.
+    event.preventDefault(); // Evita que a página recarregue.
+    navigator.clipboard.writeText(msg);
+});
+
 
 function criptografar(input){
     msg.value = input // Criptografa e imediatamente mostra na caixa de texto.
