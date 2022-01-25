fb062d62ed0894ea116ab283087c72603c32274b
JoenneTV <gabriel.natuccirusso@hotmail.com>
2022-01-25 15:08:38 -0300

    JS feito

--- js/script.js
+++ js/script.js
@@ -1,20 +1,33 @@
+var msg = document.getElementById("msg"); // Seleciona o a caixa de texto onde mostra a mensagem.
 
-/* Regras Codificador: 
-"e" é convertido para "enter" 
-"i" é convertido para "imes"
-"a" é convertido para "ai"
-"o" é convertido para "ober"
-"u" é convertido para "ufat"
-Apenas letras minúsculas
-Não permite acentuação   
-*/
+document.addEventListener("click", function(event){ // Adiciona um evento à página html.
+    input = document.getElementById("inputvalor").value;
+    event.preventDefault(); // Evita que a página recarregue.
+    let botao = event.target.value; // Lê o valor do botão que foi apertado.
+    if (botao == "Criptografar"){
+        criptografar(input);
+    } else if (botao === "Descriptografar"){
+        descriptografar(input);
+    } else if (botao == "Clique para copiar"){
+        navigator.clipboard.writeText(msg.value)
+    }
 
-/* Regras Decodificador: 
-"enter" é convertido para "e" 
-"imes" é convertido para "i"
-"ai" é convertido para "a"
-"ober" é convertido para "o"
-"ufat" é convertido para "u"
-Apenas letras minúsculas
-Não permite acentuação     
-*/
+});
+
+function criptografar(input){
+    msg.value = input // Criptografa e imediatamente mostra na caixa de texto.
+        .replace(/e/g,"enter")
+        .replace(/i/g,"imes")
+        .replace(/a/g,"ai")
+        .replace(/o/g,"ober")
+        .replace(/u/g,"ufat");
+}
+
+function descriptografar(){
+    msg.value = input // Descriptografa e imediatamente mostra na caixa de texto.
+        .replace(/enter/g,"e")
+        .replace(/imes/g,"i")
+        .replace(/ai/g,"a")
+        .replace(/ober/g,"o")
+        .replace(/ufat/g,"u");
+}
