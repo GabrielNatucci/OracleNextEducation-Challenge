cb62e16dcd6c75f005be834ba5ea648887be92df
GabrielNatucci <gabriel.natuccirusso@hotmail.com>
2022-01-29 20:03:58 -0300

    correção dos bugs adicionados no commit anterior

--- js/script.js
+++ js/script.js
@@ -3,15 +3,16 @@ var cripto = document.getElementById("btncripto");
 var descripto = document.getElementById("btndescritpo");
 var copiar = document.getElementById("copiar");
 var apagar = document.getElementById("btnapagar");
-var input = document.getElementById("inputvalor").value;
 
-cripto.addEventListener("click", function(event){ // Adiciona um evento à página html.
+cripto.addEventListener("click", function(event,input){ // Adiciona um evento à página html.
     event.preventDefault(); // Evita que a página recarregue.
+    var input = document.getElementById("inputvalor").value;
     criptografar(input);
 });
 
 descripto.addEventListener("click", function(event){ // Adiciona um evento à página html.
     event.preventDefault(); // Evita que a página recarregue.
+    var input = document.getElementById("inputvalor").value;
     descriptografar(input); 
 }); 
 
@@ -23,12 +24,12 @@ apagar.addEventListener("click", function (event) {
 copiar.addEventListener("click",function(event){
     var msg = document.getElementById("msg"); // Seleciona o a caixa de texto onde mostra a mensagem.
     event.preventDefault(); // Evita que a página recarregue.
-    navigator.clipboard.writeText(msg.innerHTML);
+    navigator.clipboard.writeText(msg.value);
 });
 
 
 function criptografar(input){
-    msg.innerHTML = input // Criptografa e imediatamente mostra na caixa de texto.
+    msg.value = input // Criptografa e imediatamente mostra na caixa de texto.
         .toLowerCase() // Tira os caractéres maíusculos
         .normalize("NFD")
         .replace(/e/g,"enter")
@@ -38,8 +39,8 @@ function criptografar(input){
         .replace(/u/g,"ufat");
 }
 
-function descriptografar(){
-    msg.innerHTML = input // Criptografa e imediatamente mostra na caixa de texto.
+function descriptografar(input){
+    msg.value = input // Criptografa e imediatamente mostra na caixa de texto.
         .toLowerCase() // Tira os caractéres maíusculos
         .normalize("NFD") // Remove acentos
         .replace(/enter/g,"e")
